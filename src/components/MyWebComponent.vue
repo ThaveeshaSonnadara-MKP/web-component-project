0092a958a9034c81dbddf69cc35cd1f78b0ff6f4
ThaveeshaSonnadara-MKP <thaveesha@marketpushapps.com>
2024-12-03 20:45:25 +0530

    v1.0

---
+++ src/components/MyWebComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <section>
+    <script
+      src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs"
+      type="module"
+      defer
+    ></script>
+    <div>
+      <div class = 'flex-start'>
+        <dotlottie-player
+          src="https://lottie.host/2e3dd6ce-3a2e-41aa-b202-58c0387499f6/Qf8L1lhHjz.lottie"
+          background="transparent"
+          speed="1"
+          style="width: 50px; height: 50px"
+          loop
+          autoplay
+        ></dotlottie-player>
+        <h1>My Vue Web Component</h1>
+      </div>
+      <div class = 'flex-start'>
+        <p>{{ msg }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["msg"],
+};
+</script>
+
+<style>
+.flex-start {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 1rem;
+  width: 100%;
+  background-color: #f2f2f2;
+  font-family: Arial, sans-serif;
+}
+h1 {
+  color: green;
+}
+p {
+  color: blue;
+}
+section {
+  background-color: oldlace;
+  margin-bottom: 4rem;
+}
+</style>
