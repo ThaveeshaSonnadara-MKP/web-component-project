726df9f1315d6fddfab2c64eaa296d992891c697
ThaveeshaSonnadara-MKP <thaveesha@marketpushapps.com>
2024-12-04 21:05:13 +0530

    Copied previous element's code and made few changes

---
+++ src/components/BackgroundElement.vue
@@ -0,0 +1,113 @@
+<template>
+    <section class="text-style">
+        <script src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs"
+            type="module"></script>
+        <div>
+            <!-- :style="CSSVariables" -->
+            <div class="flex-start" :style="{ 'background-color': bg_color }">
+                <dotlottie-player @click="iconClicked"
+                    src="https://lottie.host/2e3dd6ce-3a2e-41aa-b202-58c0387499f6/Qf8L1lhHjz.lottie"
+                    background="transparent" speed="1" style="width: 50px; height: 50px; cursor: pointer;" loop
+                    autoplay></dotlottie-player>
+                <h1 class="no-selection" :style="{ 'color': title_text_color }">{{ title.toUpperCase() }}</h1>
+            </div>
+
+            <div class="msg-box" :style="{ 'border-width': bg_border_width + 'px', 'border-color': bg_border_color }">
+                <div class="emoji" v-if="emojiname == 'bear'">🐻</div>
+                <div class="emoji" v-if="emojiname == 'fox'">🦊</div>
+                <div class="emoji" v-if="emojiname == 'bird'">🐥</div>
+                <div class="emoji" v-if="emojiname == 'unicorn'">🦄</div>
+
+                <p :style="{ 'color': msg_text_color }">
+                    {{ msg }}</p>
+            </div>
+
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "background-element",
+    props: {
+        bg_color: {
+            type: String,
+            default: "rgb(220, 205, 235)",
+        },
+        title_text_color: {
+            type: String,
+            default: "black",
+        },
+        msg_text_color: {
+            type: String,
+            default: "black",
+        },
+        bg_border_width: {
+            type: Number,
+            default: 0,
+        },
+        bg_border_color: {
+            type: String,
+            default: "#CF7C1B",
+        },
+        msg: {
+            type: String,
+            required: true,
+            default: "Welcome to MKP Dev's Custom Widget! 🙌🧿",
+        },
+        title: {
+            type: String,
+            default: "A Component By MKP Dev Team.",
+        },
+        emojiname: {
+            type: String,
+            default: "bear",
+            validate: true,
+            validator: function (value) { 
+                return ["bear", "fox", "bird", "unicorn"].includes(value);
+            }
+        },
+    },
+    methods: {
+        iconClicked() {
+            location.reload();
+        }
+    },
+};
+</script>
+
+<style scoped>
+.flex-start {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 1rem;
+    width: 100%;
+}
+
+.msg-box {
+    margin: 10px;
+    gap: 10px;
+    padding: 0.5rem;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    border-style: solid;
+}
+
+.text-style {
+    font-family: Arial, sans-serif;
+}
+
+.no-selection {
+    cursor: default;
+    -webkit-user-select: none; /* Safari */
+    user-select: none;
+}
+
+.emoji {
+    cursor: pointer;
+    user-select: auto;
+    font-size: 2rem;
+}
+</style>
