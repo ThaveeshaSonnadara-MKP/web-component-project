6f97b89b3b77a90742fbc9c5dd4a013eb5ceee19
ThaveeshaSonnadara-MKP <thaveesha@marketpushapps.com>
2024-12-05 11:33:15 +0530

    Updated all the configs (dragged from a working project sumudu's repo)

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,5 @@
-// module.exports = {
-//   presets: [
-//     '@vue/cli-plugin-babel/preset'
-//   ]
-// }
 module.exports = {
-  presets: ["@babel/preset-env"],
-};
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
--- vue.config.js
+++ vue.config.js
@@ -1,48 +1,48 @@
-function enableShadowCss(config) {
-  const configs = [
-    config.module.rule("vue").use("vue-loader"),
-    config.module.rule("css").oneOf("vue-modules").use("vue-style-loader"),
-    config.module.rule("css").oneOf("vue").use("vue-style-loader"),
-    config.module.rule("css").oneOf("normal-modules").use("vue-style-loader"),
-    config.module.rule("css").oneOf("normal").use("vue-style-loader"),
-    config.module.rule("postcss").oneOf("vue-modules").use("vue-style-loader"),
-    config.module.rule("postcss").oneOf("vue").use("vue-style-loader"),
-    config.module
-      .rule("postcss")
-      .oneOf("normal-modules")
-      .use("vue-style-loader"),
-    config.module.rule("postcss").oneOf("normal").use("vue-style-loader"),
-    config.module.rule("scss").oneOf("vue-modules").use("vue-style-loader"),
-    config.module.rule("scss").oneOf("vue").use("vue-style-loader"),
-    config.module.rule("scss").oneOf("normal-modules").use("vue-style-loader"),
-    config.module.rule("scss").oneOf("normal").use("vue-style-loader"),
-    config.module.rule("sass").oneOf("vue-modules").use("vue-style-loader"),
-    config.module.rule("sass").oneOf("vue").use("vue-style-loader"),
-    config.module.rule("sass").oneOf("normal-modules").use("vue-style-loader"),
-    config.module.rule("sass").oneOf("normal").use("vue-style-loader"),
-    config.module.rule("less").oneOf("vue-modules").use("vue-style-loader"),
-    config.module.rule("less").oneOf("vue").use("vue-style-loader"),
-    config.module.rule("less").oneOf("normal-modules").use("vue-style-loader"),
-    config.module.rule("less").oneOf("normal").use("vue-style-loader"),
-    config.module.rule("stylus").oneOf("vue-modules").use("vue-style-loader"),
-    config.module.rule("stylus").oneOf("vue").use("vue-style-loader"),
-    config.module
-      .rule("stylus")
-      .oneOf("normal-modules")
-      .use("vue-style-loader"),
-    config.module.rule("stylus").oneOf("normal").use("vue-style-loader"),
-  ];
-  configs.forEach((c) =>
-    c.tap((options) => {
-      options.shadowMode = true;
-      return options;
-    })
-  );
-}
-
+const webpack = require("webpack");
 module.exports = {
-  // https://cli.vuejs.org/guide/webpack.html#chaining-advanced
+  css: {
+    extract: false,
+    // modules: true,
+    loaderOptions: {
+      scss: {
+        // additionalData: `@import "~@/styles/global.scss";`,
+      },
+    },
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      }),
+    ],
+  },
+  filenameHashing: false,
   chainWebpack: (config) => {
-    enableShadowCss(config);
+    config.optimization.delete("splitChunks");
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap((options) => {
+        options.shadowMode = true;
+        return options;
+      });
+    config.module
+      .rule("css")
+      .oneOf("vue")
+      .use("vue-style-loader")
+      .tap((options) => {
+        options.shadowMode = true;
+        return options;
+      });
+    config.module
+      .rule("scss")
+      .oneOf("vue")
+      .use("vue-style-loader")
+      .tap((options) => {
+        options.shadowMode = true;
+        return options;
+      });
+    // enableShadowCss(config);
   },
 };
